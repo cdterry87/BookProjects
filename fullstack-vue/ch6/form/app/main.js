bddd3dc28baccfd19ff85018421d2ab5f3b9319a
Chase Terry <chaset@ChaseMM.local>
2018-10-29 10:07:18 -0500

    Added local storage to the form application in Chapter 6 of Fullstack Vue.

--- fullstack-vue/ch6/form/app/main.js
+++ fullstack-vue/ch6/form/app/main.js
@@ -1,3 +1,28 @@
+let apiClient = {
+    loadItems: function() {
+        return {
+            then: function(cb) {
+                setTimeout(() => {
+                    cb(JSON.parse(localStorage.items || '[]'));
+                }, 1000);
+            }
+        };
+    },
+    saveItems: function(items) {
+        const success = !!(this.count++ % 2);
+
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                if (!success) return reject ({ success });
+
+                localStorage.items = JSON.stringify(items);
+                return resolve({ success });
+            }, 1000);
+        });
+    },
+    count: 1
+}
+
 const buttonRow = {
     template: `<div>
         <button @click="onButtonClick" name="button-hoodie" value="fullstack-hoodie" class="ui button">Hoodie</button>
@@ -20,6 +45,9 @@ const inputForm = {
                 <label>New Item</label>
                 <input v-model="fields.newItem" type="text" placeholder="Add an item!">
                 <span style="color: red">{{ fieldErrors.newItem }}</span>
+                <span v-if="isNewItemInputLimitExceeded" style="color: red; display: block;">
+                    Must be under twenty characters!
+                </span>
             </div>
             <div class="field">
                 <label>Email</label>
@@ -35,6 +63,9 @@ const inputForm = {
                     <option>Urgent</option>
                 </select>
                 <span style="color: red">{{ fieldErrors.urgency }}</span>
+                <span v-if="isNotUrgent" style="color: red; display: block;">
+                    Must be moderate to urgent!
+                </span>
             </div>
             <div class="field">
                 <div class="ui checkbox">
@@ -43,11 +74,15 @@ const inputForm = {
                     <span style="color: red">{{ fieldErrors.termsAndConditions }}</span>
                 </div>
             </div>
-            <button class="ui button">Submit</button>
+            <button v-if="saveStatus === 'SAVING'" disabled class="ui button">Saving...</button>
+            <button v-if="saveStatus === 'SUCCESS'" :disabled="isNewItemInputLimitExceeded || isNotUrgent" class="ui button">Saved! Submit another!</button>
+            <button v-if="saveStatus === 'ERROR'" :disabled="isNewItemInputLimitExceeded || isNotUrgent" class="ui button">Save Failed - Retry?</button>
+            <button v-if="saveStatus === 'READY'" :disabled="isNewItemInputLimitExceeded || isNotUrgent" class="ui button">Submit</button>
         </form>
         <div class="ui segment">
             <h4 class="ui header">Items</h4>
             <ul>
+                <div v-if="loading" class="ui active inline loader"></div>
                 <li v-for="item in items" class="item">{{ item }}</li>
             </ul>
         </div>
@@ -67,6 +102,23 @@ const inputForm = {
                 termsAndConditions: undefined
             },
             items: [],
+            loading: false,
+            saveStatus: 'READY'
+        }
+    },
+    created() {
+        this.loading = true;
+        apiClient.loadItems().then((items) => {
+            this.items = items;
+            this.loading = false;
+        });
+    },
+    computed: {
+        isNewItemInputLimitExceeded() {
+            return this.fields.newItem.length >= 20;
+        },
+        isNotUrgent() {
+            return this.fields.urgency === 'Nonessential';
         }
     },
     methods: {
@@ -76,11 +128,22 @@ const inputForm = {
             this.fieldErrors = this.validateForm(this.fields);
             if (Object.keys(this.fieldErrors).length) return;
 
-            this.items.push(this.fields.newItem);
-            this.fields.newItem = '';
-            this.fields.email = '';
-            this.fields.urgency = '';
-            this.fields.termsAndConditions = false;
+            const items = [...this.items, this.fields.newItem];
+            this.saveStatus = 'SAVING';
+            apiClient.saveItems(items)
+                .then(() => {
+                    // this.items.push(this.fields.newItem);
+                    this.items = items;
+                    this.fields.newItem = '';
+                    this.fields.email = '';
+                    this.fields.urgency = '';
+                    this.fields.termsAndConditions = false;
+                    this.saveStatus = 'SUCCESS';
+                })
+                .catch((error) => {
+                    console.log(error);
+                    this.saveStatus = 'ERROR';
+                });
         },
         validateForm(fields) {
             const errors = {};
