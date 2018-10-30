fe5196de7851ccedf8ebd082ffdbae97f6e9e4d9
Chase Terry <chaset@ChaseMM.local>
2018-10-30 13:24:26 -0500

    Added dynamic route for Product Item page in Chapter 7 of Fullstack Vue

---
+++ fullstack-vue/ch7/cart/src/app/components/product/ProductItem.vue
@@ -0,0 +1,77 @@
+<template>
+    <section id="product-item" class="box">
+        <div class="product-item__details">
+            <h1 class="title is-4">
+                <p>{{ productItem.title }}</p>
+                <span class="tag product-item__tag">
+                    {{ productItem.product_type }}
+                </span>
+            </h1>
+            <p class="product-item__description">{{ productItem.description }}</p>
+            <p class="product-item__created_at">
+                Added: <span class="has-text-weight-bold">{{ productItem.created_at }}</span>
+            </p>
+            <button class="button is-primary product-item__button">Add to Cart</button>
+        </div>
+        <div class="product-item__image">
+            <img :src="require(`../../assets/${productItem.image_tag}`)" :alt="productItem.title" :title="productItem.title" />            
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'ProductItem',
+    props: ['id'],
+    computed: {
+        productItem() {
+            return this.$store.getters.productItemFromId(Number(this.id));
+        }
+    }
+}
+</script>
+
+<style scoped>
+#product-item {
+    display: flex;
+    width: 100%;
+    position: relative;
+}
+
+.return-icon {
+    position: absolute;
+    top: 5px;
+    left: 10px;
+    color: #00d1b2;
+    cursor: pointer;
+}
+
+.product-item__details {
+    max-width: 50%;
+    padding-left: 10px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+}
+
+.product-item__image {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+}
+
+.product-item__description {
+    padding-bottom: 10px;
+}
+
+.product-item__created_at {
+    font-size: 12px;
+    padding-bottom: 10px;
+}
+
+.product-item__button {
+    max-width: 150px;
+}
+
+
+</style>
