54d48463dc00df21fd4550ba3fd48f4b29b7bc54
= <chase.terry87@gmail.com>
2018-10-30 22:28:47 -0500

    Completed the Cart application with Vue Router.  About to add authentication.

--- fullstack-vue/ch7/cart/src/app/components/product/ProductListItem.vue
+++ fullstack-vue/ch7/cart/src/app/components/product/ProductListItem.vue
@@ -1,11 +1,14 @@
 <template>
   <div>
-    <h2 class="has-text-weight-bold">{{ productItem.title }} 
-      <span
-        @click="addCartItem(productItem)"
-        class="tag is-primary is-pulled-right has-text-white">
-        Add to Cart
-      </span>
+    <h2 class="has-text-weight-bold">
+        <router-link :to="'/products/' + productItem.id">
+            {{ productItem.title }} 
+        </router-link>
+        <span
+            @click="addCartItem(productItem)"
+            class="tag is-primary is-pulled-right has-text-white">
+            Add to Cart
+        </span>
     </h2>
     <p>{{ productItem.description }}</p>
     <span class="has-text-primary has-text-weight-bold">
@@ -15,21 +18,19 @@
 </template>
 
 <script>
-import { mapActions } from 'vuex';
+import { mapActions } from "vuex";
 
 export default {
-  name: 'ProductListItem',
-  props: ['productItem'],
-  methods: {
-    ...mapActions([
-      'addCartItem'
-    ])
-  }
-}
+    name: "ProductListItem",
+    props: ["productItem"],
+    methods: {
+        ...mapActions(["addCartItem"])
+    }
+};
 </script>
 
 <style scoped>
 .tag {
-  cursor: pointer;
+    cursor: pointer;
 }
 </style>
