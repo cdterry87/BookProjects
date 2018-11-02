65390e60dcba95c3a2b5543d84098262472c3228
Chase Terry <chaset@ChaseMM.local>
2018-11-02 16:30:42 -0500

    Completed the Voting app in Chapter 1 of Fullstack React.

--- fullstack-react/ch1/voting-app/public/js/app.js
+++ fullstack-react/ch1/voting-app/public/js/app.js
@@ -1,54 +1,94 @@
+/* eslint-disable no-param-reassign, operator-assignment */
 
 class ProductList extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            products: []
+        };
+
+        this.handleProductUpVote = this.handleProductUpVote.bind(this);
+    }
+
+    componentDidMount() {
+        this.setState({ products: Seed.products });
+    }
+
+
+    handleProductUpVote(productId) {
+        const nextProducts = this.state.products.map(product => {
+            if (product.id === productId) {
+                return Object.assign({}, product, {
+                    votes: product.votes + 1
+                });
+            } else {
+                return product;
+            }
+        });
+        this.setState({
+            products: nextProducts
+        });
+    }
+
     render() {
-        const products = Seed.products.sort((a, b) => (
-            b.votes - a.votes
-        ));
-        const productComponents = Seed.products.map((product) => (
+        const products = this.state.products.sort((a, b) => b.votes - a.votes);
+        const productComponents = products.map(product => (
             <Product
+                key={"product-" + product.id}
                 id={product.id}
                 title={product.title}
                 description={product.description}
                 url={product.url}
                 votes={product.votes}
                 submitterAvatarUrl={product.submitterAvatarUrl}
-                productImageUrl={product.productImageUrl} />
+                productImageUrl={product.productImageUrl}
+                onVote={this.handleProductUpVote}
+            />
         ));
-        return (
-            <div className='ui unstackable items'>
-                {productComponents}
-            </div>
-        )
+        return <div className="ui unstackable items">{productComponents}</div>;
     }
 }
 
 class Product extends React.Component {
+    constructor(props) {
+        super(props);
+        
+        this.handleUpVote = this.handleUpVote.bind(this);
+    }
+
+    handleUpVote() {
+        this.props.onVote(this.props.id);
+    }
+
     render() {
         return (
-            <div className='item'>
-                <div className='image'>
+            <div className="item">
+                <div className="image">
                     <img src={this.props.productImageUrl} />
                 </div>
-                <div className='middle aligned content'>
-                    <div className='header'>
-                        <a><i className='large caret up icon' /></a>
+                <div className="middle aligned content">
+                    <div className="header">
+                        <a onClick={this.handleUpVote}>
+                            <i className="large caret up icon" />
+                        </a>
                         {this.props.votes}
                     </div>
-                    <div className='description'>
+                    <div className="description">
                         <a href={this.props.url}>{this.props.title}</a>
                         <p>{this.props.description}</p>
                     </div>
-                    <div className='extra'>
+                    <div className="extra">
                         <span>Submitted by:</span>
-                        <img className='ui avatar image' src={this.props.submitterAvatarUrl} />
+                        <img
+                            className="ui avatar image"
+                            src={this.props.submitterAvatarUrl}
+                        />
                     </div>
                 </div>
             </div>
-        )
+        );
     }
 }
 
-ReactDOM.render(
-    <ProductList />,
-    document.getElementById('content')
-)
+ReactDOM.render(<ProductList />, document.getElementById("content"));
